24a590269ef3b7409aeb7318846b198c7ae7f108
mrc-g <mrc-g@users.noreply.github.com>
2020-11-23 12:38:53 +0100

    fixed celery and path to users

--- users/app.py
+++ users/app.py
@@ -104,7 +104,7 @@ def get_config(configuration=None):
 def setup(application, config):
     if config["REMOVE_DB"]:  # remove the db file
         try:
-            os.remove("bookings/" + config["SQLALCHEMY_DATABASE_URI"])
+            os.remove("users/" + config["SQLALCHEMY_DATABASE_URI"])
             logging.info("- GoOutSafe: Users Database Removed") # pragma: no cover
         except:
             pass
@@ -159,8 +159,8 @@ def create_worker_app():
     for k,v in conf.items():
         application.config[k] = v # insert the requested configuration in the app configuration
 
-    
     db.init_app(application)
+    init_celery(application)
 
     return application
 
--- users/background.py
+++ users/background.py
@@ -36,6 +36,7 @@ def init_celery(app, worker=False):
     # load celery config
     celery.config_from_object(app.config)
     global _APP
+    print("asdasdsadsadasd\n\n\n\n")
     _APP = app
     if not worker:
         # Config for non-worker related settings
